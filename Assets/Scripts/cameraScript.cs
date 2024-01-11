d99e60ff0e0b1d968fc9434bc82205a9808c3ebe
AndresGaviXD <agaviria1@udi.edu.co>
2024-01-11 17:49:32 -0500

    Actualizacion Camara y mapa

--- Assets/Scripts/cameraScript.cs
+++ Assets/Scripts/cameraScript.cs
@@ -6,11 +6,55 @@ public class cameraScript : MonoBehaviour
 {
     public GameObject jugador;
 
+    private float target_posX;
+    private float target_posY;
+
+    private float posX;
+    private float posY;
+
+    public float derechaMax;
+    public float izquierdaMax;
+
+    public float alturaMax;
+    public float alturaMin;
+
+    public float speed;
+    public bool encendida = true;
+
+    void Awake()
+    {
+        posX = target_posX + derechaMax;
+        posY = target_posY + alturaMin;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(posX, posY, -1),1);
+    }
+
+    void Move_Cam()
+    {
+        if (encendida)
+        {
+            if (jugador)
+            {
+                target_posX = jugador.transform.position.x;
+                target_posY = jugador.transform.position.y;
+
+                if (target_posX > derechaMax && target_posX < izquierdaMax)
+                {
+                    posX = target_posX;
+
+                }
+                if (target_posY < alturaMax && target_posY > alturaMin)
+                {
+                    posY = target_posY;
+                }
+            }
+
+            transform.position = Vector3.Lerp(transform.position, new Vector3(posX, posY, -1), speed*Time.deltaTime);
+        }
+    }
+
 
     void Update()
     {
-        Vector3 position = transform.position;
-        position.x = jugador.transform.position.x;
-        transform.position = position;
+        Move_Cam();
     }
 }
