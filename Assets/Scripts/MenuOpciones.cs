0bcb5cdcdbecdf0fae2e73086767530d8a1da9cf
AndresGaviXD <agaviria1@udi.edu.co>
2024-01-16 18:19:13 -0500

    menú opciones 2

--- Assets/Scripts/MenuOpciones.cs
+++ Assets/Scripts/MenuOpciones.cs
@@ -17,4 +17,9 @@ public class MenuOpciones : MonoBehaviour
         audioMixer.SetFloat("Volumen", volumen);
 
     }
+
+    public void CambiarCalidad(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+    }
 }
