ee84710a7e7713614e2e448f3e06f0135f2d54f7
AndresGaviXD <agaviria1@udi.edu.co>
2024-04-11 20:31:57 -0500

    jee

--- Assets/Scripts/veinteGameManager.cs
+++ Assets/Scripts/veinteGameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class veinteGameManager : GameManager
 {
     public CasillaTablero tablero;
+    public CanvasGroup gameOver;
 
     private void Start()
     {
@@ -13,6 +14,9 @@ public class veinteGameManager : GameManager
 
     public void NewGame()
     {
+        gameOver.alpha = 0f;
+        gameOver.interactable = false;
+
         tablero.ClearTablero();
         tablero.CreateCasilla();
         tablero.CreateCasilla();
@@ -22,6 +26,25 @@ public class veinteGameManager : GameManager
     public void GameOver()
     {
         tablero.enabled = false;
+        gameOver.interactable = true;
+        StartCoroutine(Fade(gameOver, 1f, 1f));
+    }
+
+    private IEnumerator Fade(CanvasGroup canvasGroup, float to, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        float elapsed = 0f;
+        float duration = 0.5f;
+        float from = canvasGroup.alpha;
+
+        while (elapsed < duration)
+        {
+            canvasGroup.alpha = Mathf.Lerp(from, to, elapsed/duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        canvasGroup.alpha = to;
     }
 
 }
