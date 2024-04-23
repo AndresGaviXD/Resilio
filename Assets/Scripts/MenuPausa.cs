6a2124ff32ab7f6468901edeb706f76a975df18a
AndresGaviXD <agaviria1@udi.edu.co>
2024-04-22 20:30:21 -0500

    menpu pausa adelanto 2

--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class MenuPausa : MonoBehaviour
 {
+
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject menuPausa;
+
     public void Pausa()
     {
         Time.timeScale = 0f;
+        botonPausa.SetActive(false);
+        menuPausa.SetActive(true);
+    }
+
+    public void Reanudar()
+    {
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
     }
 }
